ea779627b60d8c0b9dc44540d50380a262559503
timothyngcy <timothyngcongyao@gmail.com>
2025-01-15 18:13:08 +0800

    Added Links

--- helloworld/home/views.py
+++ helloworld/home/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render, HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'home/base.html')
+
+def schedule(request):
+    return render(request, 'home/schedule.html')
+
+def tasks(request):
+    return render(request, 'home/tasks.html')
+
+def mail(request):
+    return render(request, 'home/mail.html')
